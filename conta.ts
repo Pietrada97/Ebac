729896b4eb6a6bdf547fee271230255fd74148cb
Pietro Siqueira <127627344+Pietrada97@users.noreply.github.com>
2024-07-04 07:02:21 -0300

    Add files via upload

---
+++ conta.ts
@@ -0,0 +1,16 @@
+
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+
+function greet(name: string): string {
+    return "Olá " + name;
+}
+
+
+const result = multiply(3, 4);
+const greeting = greet("Pietro");
+
+console.log(result); 
+console.log(greeting); 
